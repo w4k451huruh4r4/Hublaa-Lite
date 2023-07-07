8bc1b7710786019f87eefac03152f87501891c08
Mohamad Firdaus <30754820+w4k451huruh4r4@users.noreply.github.com>
2023-07-08 01:39:49 +0800

    Update me.php

--- me.php
+++ me.php
@@ -2,5 +2,5 @@
 header('Content-Type: application/json');
 require 'fbGraphExplorer.php';
 parse_str($_SERVER['QUERY_STRING'], $queryArray);
-echo fbGraphExplorer('GET', '/me', $queryArray, false);
+echo FBapi('GET', '/me', $queryArray, false);
 ?>
