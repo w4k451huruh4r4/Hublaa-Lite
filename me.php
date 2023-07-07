2446228a4d108feae4fbed6231e7781dc7853067
Mohamad Firdaus <30754820+w4k451huruh4r4@users.noreply.github.com>
2023-07-08 02:17:59 +0800

    Update me.php

--- me.php
+++ me.php
@@ -1,6 +1,6 @@
 <?php 
 header('Content-Type: application/json');
-require 'function/HublaaApi.php';
+require 'function/HublaaAPI.php';
 parse_str($_SERVER['QUERY_STRING'], $queryArray);
 echo HublaaAPI('/me', 'GET', $queryArray, false);
 ?>
