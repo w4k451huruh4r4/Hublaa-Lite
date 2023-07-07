6916aa57b9ff33a47d6ee7c67f115341cae78278
Mohamad Firdaus <30754820+w4k451huruh4r4@users.noreply.github.com>
2023-07-08 01:08:35 +0800

    Create fbGraphExplorer.php

---
+++ fbGraphExplorer.php
@@ -0,0 +1,36 @@
+<?php 
+function fbGraphExplorer($method, $url, $data) {
+$curl = curl_init();
+switch ($method) {
+case "POST":
+curl_setopt($curl, CURLOPT_POST, 1);
+if ($data)
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+break;
+case "PUT":
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+if ($data)
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+break;
+default:
+if ($data)
+$url = sprintf("%s?%s", $url, http_build_query($data));
+}
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+'Content-Type: application/json',
+));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+$result = curl_exec($curl);
+if($result) {
+$decode = json_decode($result, true);
+return json_encode($decode,JSON_PRETTY_PRINT);
+} else {
+$error = array("error_code"=>1000,"error_msg"=>"Unable to connect Facebook server (1000)");
+return json_encode($error,JSON_PRETTY_PRINT);
+}
+curl_close($curl);
+}
+?>
