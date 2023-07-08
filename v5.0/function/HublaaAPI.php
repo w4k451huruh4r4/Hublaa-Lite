95150baaf7505c51dc770c00c037eaf47cd7c1f5
Mohamad Firdaus <30754820+w4k451huruh4r4@users.noreply.github.com>
2023-07-08 08:54:45 +0800

    Create HublaaAPI.php

---
+++ v5.0/function/HublaaAPI.php
@@ -0,0 +1,35 @@
+<?php 
+function HublaaAPI($url, $method, $data) {
+$curl = curl_init();
+switch ($method) {
+case "POST":
+curl_setopt($curl, CURLOPT_POST, 1);
+if ($data)
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+break;
+case "PUT":
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+if ($data)
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+break;
+default:
+if ($data)
+$url = sprintf("%s?%s", "https://graph.facebook.com".$url, http_build_query($data));
+}
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+'Content-Type: application/json',
+));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+$result = curl_exec($curl);
+if($result) {
+$decode = json_decode($result, true);
+return json_encode($decode, JSON_PRETTY_PRINT);
+} else {
+$error = array("error_code"=>1000, "error_msg"=>"Hublaa unable to connect Facebook server (1000)");
+return json_encode($error, JSON_PRETTY_PRINT);
+}
+curl_close($curl);
+}
+?>
