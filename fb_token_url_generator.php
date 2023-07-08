1bb30b9c5306fa2d0ac3d6ce256a22c8afb6d0b4
Mohamad Firdaus <30754820+w4k451huruh4r4@users.noreply.github.com>
2023-07-08 08:32:29 +0800

    Update fb_token_url_generator.php

--- fb_token_url_generator.php
+++ fb_token_url_generator.php
@@ -1,7 +1,6 @@
 <?php
 header('Content-Type: application/json');
-
-function signed_request_url($email,$password) {
+function signed_request_url($email, $password) {
 $query = array(
 "api_key" => "882a8490361da98702bf97a021ddc14d",
 "credentials_type" => "password",
@@ -24,18 +23,17 @@ $sig = md5($sig);
 $query['sig'] = $sig;
 return "https://api.facebook.com/restserver.php?". http_build_query($query)."&sig=".$query['sig'];
 }
-
 if(empty($_GET['email'])) {
-$error = array("error_code"=>100,"error_msg"=>"The parameter email is required (100)");
+$error = array("error_code"=>100, "error_msg"=>"The parameter email is required (100)");
 echo json_encode($error, JSON_PRETTY_PRINT);
 } elseif(empty($_GET['password'])) {
-$error = array("error_code"=>100,"error_msg"=>"The parameter password is required (100)");
+$error = array("error_code"=>100, "error_msg"=>"The parameter password is required (100)");
 echo json_encode($error, JSON_PRETTY_PRINT);
 } elseif(strlen($_GET['email']) < 4 || strlen($_GET['email']) > 50 || strlen($_GET['password']) < 6 ) {
-$error = array("error_code"=>100,"error_msg"=>"Invalid email or password (100)");
+$error = array("error_code"=>100, "error_msg"=>"Invalid email or password (100)");
 echo json_encode($error, JSON_PRETTY_PRINT);
 } else {
-$json = array("url"=>signed_request_url($_GET['email'],$_GET['password']));
+$json = array("url"=>signed_request_url($_GET['email'], $_GET['password']));
 echo json_encode($json, JSON_PRETTY_PRINT);
 }
 ?>
